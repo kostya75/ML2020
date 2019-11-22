6ae755e73babe3f107a8739e6ca319587b520c46
Konstantin Mingoulin <kmingoulin@thekennagroup.com>
2019-11-22 16:00:40 -0500

    working wrapper gd

---
+++ gd.R
@@ -0,0 +1,72 @@
+
+#wrapper around fomula and data to get 2 design matrixies: x and y with "no-intercept" control
+# based on lm function
+
+#gradient descent function
+gd<-function(formula,data,subset,theta){
+  cl<-match.call()
+
+  mf <- match.call(expand.dots = F)
+
+  m <- match(c("formula", "data","subset"), 
+             names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  #mf <- mf[m]
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  mt <- attr(mf, "terms")
+  
+  y <- model.response(mf, "numeric")
+  x <- model.matrix(mt, mf)
+  
+  #lm.fit (x, y)$coefficients
+  tt<-list(x,y,theta)
+}
+
+
+tt_f<-function(formula,data,subset,theta){
+  cl<-match.call()
+  
+  mf <- match.call(expand.dots = F)
+  
+  m <- match(c("formula", "data","subset"), 
+              names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  
+  # 
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  mt <- attr(mf, "terms")
+  # 
+  y <- model.response(mf, "numeric")
+  x <- model.matrix(mt, mf)
+  theta
+  # 
+  # 
+  tt<-list(x,y,theta)
+  #lm.fit (x, y)$coefficients
+}
+
+tt<-tt_f(price~bedroom+footage-1,data=df_prices,subset=10:47,theta=c(1,2))
+tt
+
+#returns specified by full name
+#tt_f(formula = price ~ footage + bedroom, data = df_price)
+
+# mf : at that point
+# tt_f(formula = price ~ footage + bedroom - 1, data = df_prices, 
+#      drop.unused.levels = TRUE)
+# 
+# string again
+# > tt[[1]]<-quote(stats::model.frame)
+# > tt
+# tt<-stats::model.frame(formula = price ~ footage + bedroom, data = df_prices, 
+#                    drop.unused.levels = TRUE)
+# Data added
+# eval(tt, parent.frame())
+
+tt<-gd(price~bedroom+footage-1,data=df_prices,subset=10:47,theta=c(1,2))
+
+lm(price~bedroom+footage-1,data=df_prices,subset=10:47)
--- grad_des.R
+++ grad_des.R
@@ -30,3 +30,33 @@ print(J)
 x mxn
 theta nx1
 y mx1
+
+# compute cost
+# unlike computeCost, computeCost2 does not add column of 1 for the intercept as this is hanled by gd function in the same way as stat::lm does
+
+computeCost2<-function(X, y, theta){
+  X<-as.matrix(X)
+  y<-as.matrix(y)
+  theta<-as.matrix(theta)
+  # m<-dim(X)[1]
+  # X<-cbind(rep(1,m),X)
+  J<-1/(2*m)*sum((X%*%theta-y)^2)
+}
+
+gradientDescentMulti<-function(X, y, theta, alpha, num_iters){
+  J_history<-vector(mode = "numeric", length = num_iters)
+  m<-nrow(X)
+  for(i in seq_len(num_iters)){
+    theta<-theta-alpha/m*(t(X)*(X%*%theta-y))
+    J_history[i]<-computeCost2(X, y, theta)
+  }
+  J_history
+}
+  
+ tt<-gradientDescentMulti (X1,y1,theta[1:2,1],.05,20)
+   
+ t(X1) * (X1 %*% theta[1:2,1] - y1)
+ dim(t(X1))
+ dim(theta)
+ 
+[theta, J_history]
