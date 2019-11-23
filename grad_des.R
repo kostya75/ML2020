8ca764882a7b9e637b2fc2139f217c382f407247
Konstantin Mingoulin <kmingoulin@thekennagroup.com>
2019-11-22 20:21:42 -0500

    final nov 22

--- grad_des.R
+++ grad_des.R
@@ -1,62 +1,51 @@
 
-df_prices
-
-X1<-as.matrix(df_prices[c("footage","bedroom")])
-y1<-as.matrix(df_prices["price"])
-
-# compute cost
-
-computeCost<-function(X, y, theta){
-  X<-as.matrix(X)
-  y<-as.matrix(y)
-  #have validation for numeric vector
-  theta<-as.matrix(theta)
-  
-  m<-dim(X)[1]
-  X<-cbind(rep(1,m),X)
-  J<-1/(2*m)*sum((X%*%theta-y)^2)
-  
-  
-}
-
-m1<-lm(y1~X1)
-theta<-matrix(m1$coefficients,byrow = F)
-
-
-
-J<-computeCost(X1,y1,theta)
-
-print(J)
-x mxn
-theta nx1
-y mx1
-
 # compute cost
 # unlike computeCost, computeCost2 does not add column of 1 for the intercept as this is hanled by gd function in the same way as stat::lm does
 
 computeCost2<-function(X, y, theta){
-  X<-as.matrix(X)
-  y<-as.matrix(y)
-  theta<-as.matrix(theta)
-  # m<-dim(X)[1]
+  # X<-as.matrix(X)
+  # y<-as.matrix(y)
+  # theta<-as.matrix(theta)
+  m<-dim(X)[1]
   # X<-cbind(rep(1,m),X)
-  J<-1/(2*m)*sum((X%*%theta-y)^2)
+  J<-(1/(2*m))*sum((X%*%theta-y)^2)
 }
 
 gradientDescentMulti<-function(X, y, theta, alpha, num_iters){
   J_history<-vector(mode = "numeric", length = num_iters)
   m<-nrow(X)
   for(i in seq_len(num_iters)){
-    theta<-theta-alpha/m*(t(X)*(X%*%theta-y))
+    theta<-theta-alpha/m*(t(X)%*%(X%*%theta-y))
     J_history[i]<-computeCost2(X, y, theta)
   }
   J_history
 }
   
- tt<-gradientDescentMulti (X1,y1,theta[1:2,1],.05,20)
-   
- t(X1) * (X1 %*% theta[1:2,1] - y1)
- dim(t(X1))
- dim(theta)
- 
-[theta, J_history]
+
+
+
+tt<-lapply(tt,as.matrix)
+lapply(tt,dim)
+x1<-tt[["x"]]
+y1<-tt[["y"]]
+theta1<-tt[["theta"]]
+
+J<-gradientDescentMulti(x1,y1,theta1,alpha=.05,10)
+cc<-computeCost2(x1, y1, theta1)
+
+
+theta1-t(x1)%*%(x1%*%theta1-y1)
+
+
+num_iters=100
+J_history<-vector(mode = "numeric", length = num_iters)
+X=x1
+y=y1
+theta=theta1
+alpha=0.5
+m<-nrow(X)
+for(i in seq_len(num_iters)){
+  theta<-theta-alpha/m*(t(X)%*%(X%*%theta-y))
+  J_history[i]<-computeCost2(X, y, theta)
+}
+J_history
