ce62bf80b467089a5c7ac32f843048bb7648a91b
Konstantin Mingoulin <kmingoulin@thekennagroup.com>
2019-12-17 17:07:25 -0500

    dec 17

---
+++ OneVsAll.R
@@ -0,0 +1,150 @@
+#oneVsall
+
+########## testing#########
+
+#df_numbers<-do.call(,data_numbers)
+df_numbers<-data.frame(data_numbers)
+
+theta<-rep(0,401)
+hr_all<-gdlregMulti(y~.,data=df_numbers,theta=theta,lambda=0,method="BFGS")
+
+
+X2<-df_numbers[,-401]
+y1<-df_numbers[,401]
+hr_all_predict<-predictOneVsAll(hr_all[["all_theta"]],X2,hr_all[["y_class"]])
+
+# all
+confusion_matrix<-prop.table(table(y1,hr_all_predict),1)
+
+# accuracy all
+sum(hr_all_predict==y1)/length(y1)
+
+
+# training index
+train_in<-sample(5000,4000)
+
+# training set
+X_train<-df_numbers[train_in,-401]
+y_train<-df_numbers[train_in,401]
+
+# test set
+X_test<-df_numbers[-train_in,-401]
+y_test<-df_numbers[-train_in,401]
+
+accuracy<-NULL
+for (i in 0:20){
+# train the model
+hr_train<-gdlregMulti(y~.,data=df_numbers,subset=train_in,theta=theta,lambda=i,method="BFGS")
+
+# test
+hr_test_predict<-predictOneVsAll(hr_train[["all_theta"]],X_test,hr_train[["y_class"]])
+
+# accuracy test
+temp_acc<-sum(hr_test_predict==y_test)/length(y_test)
+accuracy<-c(accuracy,temp_acc)
+}
+
+
+
+########## testing#########
+
+
+
+# sigmoid function
+sigmoid<-function(z){
+  g<-1/(1+exp(-z))
+}
+
+
+# reguralize temp
+
+# CLOSURE
+ComputeCostGradient<-function(type){
+  
+  function(X, y, theta, infl, lambda){
+    # length of theta or design matrix
+    n<-dim(X)[2]
+    # number of observations
+    m<-dim(X)[1]
+    # vector to drop Xo from regularization component. check if Xo supplied to the model formula
+    if(infl==1) 
+      lambda_vector<-c(0,rep(1,n-1))
+    else 
+      lambda_vector<-c(rep(1,n))
+    
+    if(type=="J"){
+      (-1/m)*sum(y*log(sigmoid(X%*%theta))+(1-y)*log(1-sigmoid(X%*%theta)))+lambda/(2*m)*lambda_vector%*%theta^2
+    }
+    else if(type=="grad"){
+      as.numeric((1/m)*t(X)%*%(sigmoid(X%*%theta)-y)+lambda/m*lambda_vector*theta)
+    }
+    else stop("Invalid output request from CostGradient: acceptable values are: 'J' and 'grad'")
+  }
+}
+
+
+
+
+###################################################
+# gradient descent analytical
+
+gdlregMulti<-function(formula,data,subset,theta, lambda=0, method ="BFGS"){
+  #method BFGS works with a large number of features
+  
+  if(is.na(match(method,c("Nelder-Mead", "BFGS", "L-BFGS-B")))) 
+    stop("Please select on of the tested methods: Nelder-Mead, BFGS, L-BFGS-B")
+  
+  mf <- match.call(expand.dots = F)
+  m <- match(c("formula", "data","subset"), 
+             names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  #mf <- mf[m]
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  mt <- attr(mf, "terms")
+  
+  ymulti <- model.response(mf, "numeric")
+  y_class<-unique(ymulti)
+  x <- model.matrix(mt, mf)
+  # flag if intercept was selected. will set lambda vector[1] (regularization variable) to zero if intercept present in formula
+  infl <- attr(mt,"intercept")
+  #
+  n<-ncol(x)
+  if(n!=length(theta)) 
+    stop("Model formula and initial theta have incompatible dimensions")
+  # two closures are created by ComputeCostGradient function for J and grad
+  
+  all_theta<-NULL
+  for (i in y_class){
+    y<-as.numeric(ymulti %in% i)
+     J<-ComputeCostGradient("J")
+     grad<-ComputeCostGradient("grad") 
+     res<-optim(theta,J,grad,X=x,y=y,infl=infl,lambda=lambda,method=method)$par
+     #all_theta<-data.frame(rbind(all_theta,c(class=i,res)))
+     all_theta<-rbind(all_theta,res)
+     colnames(all_theta)<-colnames(x)
+     rownames(all_theta)<-NULL
+  }
+    
+
+  
+  out<-list(all_theta=all_theta,y_class=y_class)
+
+}
+
+
+#########################
+#predict
+#this function requires an additional input that insures that labels are correctly assigned to each class. More robust in my opinion
+predictOneVsAll<-function(all_theta, X, y_class){
+  if("(Intercept)" %in% colnames(all_theta)){
+    X<-data.frame(temp=1,X)
+    names(X)[1] <- "(Intercept)"
+  }
+  X<-sigmoid(as.matrix(X)%*%t(all_theta))
+  X<-apply(X,1,function(x) y_class[which.max(x)])
+}
+
+
+
--- Testing.R
+++ Testing.R
@@ -2,22 +2,60 @@
 
 #testing
 
-#week 2
+###################### week2 ####################
 
 
+library(ggplot2)
+
+ggplot(data=df_prices)+geom_point((aes(x=footage,y=price)),shape=4,color="red",size=3)+
+  scale_y_continuous(labels = scales::dollar)+
+  scale_x_continuous(labels = scales::comma)+
+  labs(x="Square footage", y="Price in $",title="House Prices vs. Square Footage")+
+  theme_bw()
 
 
+#cost
+df_foodTruck<-read.csv("w2/ex1data1.txt",header = FALSE)
+names(df_foodTruck)<-c("X","y")
 
-tt<-gd(price~bedroom+footage,data=df_prices,theta=c(0,0,0),alpha=0.01,threshold=0.00000000005,normalize=T)
+ggplot(data=df_foodTruck)+geom_point((aes(x=X,y=y)),shape=4,color="red",size=3)+
+  scale_y_continuous(labels = scales::dollar)+
+  scale_x_continuous(labels = scales::comma)+
+  labs(x="Population of City in 10,000s", y="Price in $10,000s")+
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "black"))
+
+X<-as.matrix(cbind(rep(1,nrow(df_foodTruck)),df_foodTruck[,c("X")]))
+y<-as.matrix(df_foodTruck["y"])
+
+j=computeCost(X, y, theta=c(0,0))
+
+sprintf("Computed value of cost function = %s. Expected cost value (approx) 32.07",
+       round(j,digits=2))
+
+tt<-gradientDescentMulti(X,y,theta=c(0,0),alpha=.02,num_iters = 10000,threshold=5e-11)
 tt
 
-df_prices_scaled<-df_prices
-df_prices_scaled[,c("footage","bedroom")]<-lapply(df_prices[,c("footage","bedroom")],scale)
+lm(y~X,data=df_foodTruck)
 
-lm(price~bedroom+footage,data=df_prices_scaled)
+# Feature normalization
+normalize<-featureNormalize(
+  xm=as.matrix(df_prices[c("footage","bedroom")]),
+  infl=0
+  )
+
+head(normalize$x)
+
+normalize$scalingMatrix
+# tt<-gd(price~bedroom+footage,data=df_prices,theta=c(0,0,0),alpha=0.01,threshold=0.00000000005,normalize=T)
+# tt
+# 
+# df_prices_scaled<-df_prices
+# df_prices_scaled[,c("footage","bedroom")]<-lapply(df_prices[,c("footage","bedroom")],scale)
+
+#lm(price~bedroom+footage,data=df_prices_scaled)
 
 
-#week4
+###################### week4 ####################
 dfnumbers_small<-df_numbers[sample(5000,1000),]
 
 y_class<-unique(dfnumbers_small$y)
